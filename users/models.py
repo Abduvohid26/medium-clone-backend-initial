a431cc0bc70e8cfa1a20c159a651960421e65528
Abduvohid26 <abdujalilovabduvohid6@gmail.com>
2024-09-02 15:11:23 +0500

    fix

--- users/models.py
+++ users/models.py
@@ -12,7 +12,7 @@ def file_upload(instance, filename):
 
 class CustomUser(AbstractUser):
     """  This model represents a custom user. """
-    avatar = ResizedImageField(size=[300, 300], crop=['top', 'left'], upload_to='file_upload', blank=True)
+    avatar = ResizedImageField(size=[300, 300], crop=['top', 'left'], upload_to=file_upload, blank=True)
     middle_name = models.CharField(max_length=30, blank=True, null=True)
 
 
